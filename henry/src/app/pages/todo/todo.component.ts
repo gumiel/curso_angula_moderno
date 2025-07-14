4b6c15dc3bf9681bb41838aba18bfd645c40b85f
Henry Asus <prez.gumiel@gmail.com.com>
2025-07-13 23:47:14 -0400

    ultimo

---
+++ henry/src/app/pages/todo/todo.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  imports: [],
+  templateUrl: './todo.component.html',
+})
+export class TodoComponent { }
