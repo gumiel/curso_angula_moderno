db302bb78b2cd0ca9bf8518cd3df71796045f624
Henry Asus <prez.gumiel@gmail.com.com>
2025-07-19 22:41:50 -0400

    avanzando-gifs

---
+++ S6/gifs-app/src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    { 
+        path: 'dashboard',
+        loadComponent: () => 
+            import('./gifs/pages/dashboard-page/dashboard-page.component')
+    },
+    { 
+        path: 'trending',
+        loadComponent: () => 
+            import('./gifs/pages/trending-page/trending.component')
+    },
+    { 
+        path: 'search',
+        loadComponent: () => 
+            import('./gifs/pages/search-page/search-page.component')
+    },
+    {
+        path: '**',
+        redirectTo: 'dashboard'
+    }
+];
---
+++ S6/gifs-app/src/app/gifs/pages/trending-page/trending.component.ts
@@ -0,0 +1,8 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trending',
+  imports: [],
+  templateUrl: './trending.component.html',
+})
+export default class TrendingComponent { }
